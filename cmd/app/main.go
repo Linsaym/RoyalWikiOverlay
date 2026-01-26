7d0bfa2fa4b511caba474e5c432486bfebfc02fb
Lev <leva397@mail.ru>
2026-01-27 00:57:32 +0700

    delete all

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"log"
-	"os"
-)
+import "fmt"
 
 func main() {
-	//dbPath := "data/royalwiki.db"
-
-	if err := os.MkdirAll("data", 0755); err != nil {
-		log.Fatal(err)
-	}
-
-	// Запуск приложения
-	log.Println("Application started")
+	fmt.Println("123")
 }
