a70452a3fb9f38a5cde052876aae865ff6d61d4f
Lev <leva397@mail.ru>
2026-01-30 17:04:25 +0700

    add windows forms

---
+++ cmd/app/RoyalWikiOverlay/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RoyalWikiOverlay;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        button1 = new System.Windows.Forms.Button();
+        label1 = new System.Windows.Forms.Label();
+        SuspendLayout();
+        // 
+        // button1
+        // 
+        button1.Location = new System.Drawing.Point(167, 168);
+        button1.Name = "button1";
+        button1.Size = new System.Drawing.Size(125, 55);
+        button1.TabIndex = 0;
+        button1.Text = "Туц";
+        button1.UseVisualStyleBackColor = true;
+        button1.Click += button1_Click;
+        // 
+        // label1
+        // 
+        label1.Location = new System.Drawing.Point(167, 91);
+        label1.Name = "label1";
+        label1.Size = new System.Drawing.Size(125, 37);
+        label1.TabIndex = 1;
+        label1.Text = "10";
+        label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        BackColor = System.Drawing.SystemColors.ActiveBorder;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(label1);
+        Controls.Add(button1);
+        Text = "Form1";
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.Label label1;
+
+    private System.Windows.Forms.Button button1;
+
+    #endregion
+}
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("123")
+	a := app.New()
+	w := a.NewWindow("")
+
+	w.SetContent(widget.NewLabel("hello world"))
+	w.SetPadded(false)
+	w.Resize(fyne.NewSize(200, 100))
+
+	w.ShowAndRun()
 }
